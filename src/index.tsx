cfc71bccde5af8ec00468716d8e9c3cfa10ed8e6
Spanaic <54741027+Spanaic@users.noreply.github.com>
2021-01-28 22:18:49 +0900

    【完了】React Class Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,52 @@
 import React from "react";
 import { render } from "react-dom";
 
-// class構文
+// 17. class構文
+// class Human {
+//   name!: string;
+//   age!: number;
+//   constructor(initName: string, initAge: number) {
+//     this.name = initName;
+//     this.age = initAge;
+//   }
+//   callName() {
+//     console.log(this.name);
+//   }
+// }
 
-class Human {
-  name!: string;
-  age!: number;
-  constructor(initName: string, initAge: number) {
-    this.name = initName;
-    this.age = initAge;
+// const Quill = new Human("Quill", 29);
+// console.log(Quill);
+// Quill.callName();
+
+// const Tanaka = new Human("Tanaka", 40);
+// console.log(Tanaka);
+// Tanaka.callName();
+
+// 18. React Class Component
+
+type HumanProps = {
+  age: string;
+};
+
+// https://stackoverflow.com/questions/37978528/typescript-type-string-is-not-assignable-to-type
+class Human extends React.Component<HumanProps> {
+  state: {
+    name: string;
+  };
+  constructor(props: HumanProps) {
+    super(props);
+    this.state = {
+      name: "Quill"
+    };
   }
-  callName() {
-    console.log(this.name);
+  render() {
+    return (
+      <div>
+        <h2>{this.state.name}</h2>
+        <h3>{this.props.age}</h3>
+      </div>
+    );
   }
 }
 
-const Quill = new Human("Quill", 29);
-console.log(Quill);
-Quill.callName();
-
-const Tanaka = new Human("Tanaka", 40);
-console.log(Tanaka);
-Tanaka.callName();
+render(<Human age="30" />, document.getElementById("root"));
