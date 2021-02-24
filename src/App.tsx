7a83a94986fbd4876db3ce59573065a86f23e007
Spanaic <54741027+Spanaic@users.noreply.github.com>
2021-02-24 09:02:04 +0900

    【完了】connectでstoreをコンポーネントに紐付ける

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,64 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const App = () => <h2>App</h2>;
+type MapStateToProps = {
+  number: number;
+};
 
-export default App;
+type DispatchProps = {
+  type: string;
+  payload: { num: number };
+};
+
+type MapDispatchToProps = {
+  plus: (dispatch: DispatchProps) => void;
+  minus: (dispatch: DispatchProps) => void;
+};
+
+// anyの型がいくつかある
+const App = ({ number, plus, minus }) => (
+  <div>
+    <h2>App {number}</h2>
+    <button
+      onClick={() => {
+        plus(10);
+      }}
+    >
+      + 10
+    </button>
+    <button
+      onClick={() => {
+        minus(10);
+      }}
+    >
+      - 10
+    </button>
+  </div>
+);
+
+const mapStateToProps = (state: MapStateToProps) => {
+  return {
+    number: state
+  };
+};
+
+const mapDispatchToProps = (dispatch: DispatchProps): MapDispatchToProps => {
+  return {
+    plus: (num) => {
+      dispatch({
+        type: "PLUS",
+        payload: { num: num }
+      });
+    },
+    minus: (num) => {
+      dispatch({
+        type: "MINUS",
+        payload: { num: num }
+      });
+    }
+  };
+};
+
+// HOCを使用している
+// 二つ目のかっこには対象となるコンポーネントを設定する
+export default connect(mapStateToProps, mapDispatchToProps)(App);
